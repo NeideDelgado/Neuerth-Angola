bccbf826d4a1968298e0bdc39634b59f817cf304
Neide Delgado <neide.delgado@itelnet.co.ao>
2015-04-21 17:37:49 +0100

    tudo

---
+++ projeto/profi/app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+
+	@welcome = 'Profiler'
+
+  end
+end
---
+++ projeto/profi/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Profiler::Application.config.secret_key_base = '7548bd47114a5e2624ee19f30335ba5c74a324c5fb5e93f2b490480d858806ab39967f9e9bb26a1588c59cf1a2e22711a1422c8c954c5b733a63a257b217d0fc'
